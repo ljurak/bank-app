f23cdd60f069f00760f523d9a392fc2ef6736989
ljurak <lucas.jurak@gmail.com>
2020-05-08 22:16:46 +0200

    Implement AccountRepository interface

--- src/main/java/bank/account/InMemoryAccountRepository.java
+++ src/main/java/bank/account/InMemoryAccountRepository.java
@@ -1,20 +1,23 @@
-package bank;
+package bank.account;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class AccountRepository {
+public class InMemoryAccountRepository implements AccountRepository {
 
     private Map<String, Account> accounts = new HashMap<>();
 
+    @Override
     public void addAccount(Account account) {
         accounts.put(account.getCardNumber(), account);
     }
 
+    @Override
     public boolean containsAccount(String cardNumber) {
         return accounts.containsKey(cardNumber);
     }
 
+    @Override
     public Account findAccount(String cardNumber) {
         return accounts.get(cardNumber);
     }
