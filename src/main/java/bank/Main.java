036d8b2580a1c871104565f20b5a8ff75a3e188f
ljurak <lucas.jurak@gmail.com>
2020-05-08 22:24:43 +0200

    Add data storage to the program

--- src/main/java/bank/Main.java
+++ src/main/java/bank/Main.java
@@ -1,13 +1,49 @@
 package bank;
 
+import bank.account.Account;
+import bank.account.AccountRepository;
+import bank.account.AccountService;
+import bank.account.SqliteAccountRepository;
+import bank.infrastructure.ConnectionProvider;
+import bank.infrastructure.SqliteConnectionProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    private static final String CREATE_TABLE_SQL = "CREATE TABLE card (id INTEGER PRIMARY KEY, number TEXT NOT NULL, pin TEXT NOT NULL, balance INTEGER DEFAULT 0)";
+
     public static void main(String[] args) {
-        AccountService accountService = new AccountService();
+        if (args.length < 2 || !args[0].equals("-filename")) {
+            throw new IllegalArgumentException("Missing -filename argument (e.g. java -jar bank-app.jar -filename data.db)");
+        }
+
+        String filename = args[1];
+        File file = new File(filename);
+
+        ConnectionProvider connectionProvider = new SqliteConnectionProvider("jdbc:sqlite:" + filename);
+
+        try {
+            if (!file.exists()) {
+                boolean fileCreated = file.createNewFile();
+                if (fileCreated) {
+                    createDatabase(connectionProvider);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot create file: " + filename);
+            System.exit(0);
+        }
+
+        AccountRepository accountRepo = new SqliteAccountRepository(connectionProvider);
+        AccountService accountService = new AccountService(accountRepo);
 
         showMenu();
         int option;
@@ -88,6 +124,19 @@ public class Main {
             System.out.println();
         } else {
             System.out.println("Wrong card number or PIN!" + System.lineSeparator());
+        }
+    }
+
+    private static void createDatabase(ConnectionProvider connectionProvider) {
+        try (Connection connection = connectionProvider.connect()) {
+            try {
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(CREATE_TABLE_SQL);
+            } catch (SQLException e) {
+                System.out.println("Error occurred: " + e.getMessage());
+            }
+        } catch (SQLException e) {
+            System.out.println("Error occurred: " + e.getMessage());
         }
     }
 }
